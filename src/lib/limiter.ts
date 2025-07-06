c174bac3a8ca9240b1dbfb42a51753329b7f5a43
Saim Jawed <saimjawed254@gmail.com>
2025-07-06 14:59:22 +0530

    increased time for testing

--- src/lib/limiter.ts
+++ src/lib/limiter.ts
@@ -3,7 +3,7 @@ import { Ratelimit } from "@upstash/ratelimit";
 
 export const limiter = new Ratelimit({
   redis,
-  limiter: Ratelimit.fixedWindow(2, "60 s"),
+  limiter: Ratelimit.fixedWindow(1, "120 s"),
   analytics: true,
   prefix: "@ratelimit",
 });
